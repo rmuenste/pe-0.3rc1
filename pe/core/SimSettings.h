5f3b25a1a2be87c6079c5a4c502648fcb0dd6332
Raphael Münster <raphael.muenster@tu-dortmund.de>
2023-04-12 15:09:58 +0200

    Added static class SimSettings

---
+++ pe/core/SimSettings.h
@@ -0,0 +1,162 @@
+
+//=================================================================================================
+/*!
+ *  \file pe/core/Settings.h
+ *  \brief Header file for the world settings
+ *
+ *  Copyright (C) 2023 Raphael Muenster
+ *
+ *  This file is part of pe.
+ *
+ *  pe is free software: you can redistribute it and/or modify it under the terms of the GNU
+ *  General Public License as published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  pe is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with pe. If not,
+ *  see <http://www.gnu.org/licenses/>.
+ */
+//=================================================================================================
+
+#ifndef _PE_CORE_SIMSETTINGS_H_
+#define _PE_CORE_SIMSETTINGS_H_
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <pe/math/Vector3.h>
+#include <pe/system/Precision.h>
+#include <pe/util/NonCreatable.h>
+
+
+namespace pe {
+
+//=================================================================================================
+//
+//  CLASS DEFINITION
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Settings of the rigid body simulation world.
+ * \ingroup core
+ *
+ * The Settings class represents the current settings of the rigid body simulation world.
+ * It offers the functionality to acquire the current settings (as for instance the damping
+ * factor and the gravity) for all modules of the physics engine. Although it is closely
+ * coupled to the World class, it is implemented as a separate class to minimize coupling
+ * for other classes.
+ */
+class PE_PROTECTED SimSettings : private NonCreatable
+{
+public:
+   //**Get functions*******************************************************************************
+   /*!\name Get functions */
+   //@{
+   static inline int         px();
+   static inline int         py();
+   static inline int         pz();
+   static inline real        stepsize();
+//   static inline const Vec3& gravity();
+   //@}
+   //**********************************************************************************************
+
+private:
+   //**Member variables****************************************************************************
+   /*!\name Member variables */
+   //@{
+   static int px_;  //!< The number of processes in x
+   static int py_;  //!< The number of processes in y
+   static int pz_;  //!< The number of processes in z
+   static int visspacing_;  //!< The visualization spacing
+   static real stepSize_; //!< The time step size of the simulation
+//   static Vec3 gravity_;       //!< Gravity of the simulated system.
+   //@}
+   //**********************************************************************************************
+
+   //**Friend declarations*************************************************************************
+   /*! \cond PE_INTERNAL */
+   friend class World;
+   /*! \endcond */
+   //**********************************************************************************************
+};
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  GET FUNCTIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Returns the time step size
+ *
+ * \return \a The time step size
+ */
+inline real Settings::stepsize()
+{
+   return stepSize_;
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Returns the number of processes in x
+ *
+ * \return \a The number of processes in x
+ */
+inline real Settings::px()
+{
+   return px_;
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Returns the number of processes in y
+ *
+ * \return \a The number of processes in y
+ */
+inline real Settings::py()
+{
+   return py_;
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Returns the number of processes in z
+ *
+ * \return \a The number of processes in z
+ */
+inline real Settings::pz()
+{
+   return pz_;
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Returns the visualization spacing
+ *
+ * \return \a The visualization spacing
+ */
+inline real Settings::visspacing()
+{
+   return visspacing_;
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+} // namespace pe
+
+#endif
---
+++ src/core/SimSettings.cpp
@@ -0,0 +1,52 @@
+
+//=================================================================================================
+/*!
+ *  \file src/core/Settings.cpp
+ *  \brief Source file for the world settings
+ *
+ *  Copyright (C) 2023 Raphael Muenster
+ *
+ *  This file is part of pe.
+ *
+ *  pe is free software: you can redistribute it and/or modify it under the terms of the GNU
+ *  General Public License as published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  pe is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with pe. If not,
+ *  see <http://www.gnu.org/licenses/>.
+ */
+//=================================================================================================
+
+
+//*************************************************************************************************
+// Platform/compiler-specific includes
+//*************************************************************************************************
+
+#include <pe/system/WarningDisable.h>
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <pe/core/SimSettings.h>
+
+
+namespace pe {
+
+//=================================================================================================
+//
+//  DEFINITION AND INITIALIZATION OF THE STATIC MEMBER VARIABLES
+//
+//=================================================================================================
+
+real SimSettings::stepSize_( 0.001 );
+int SimSettings::px_( 1 );
+int SimSettings::py_( 1 );
+int SimSettings::pz_( 1 );
+int SimSettings::visspacing_( 50 );
+} // namespace pe
