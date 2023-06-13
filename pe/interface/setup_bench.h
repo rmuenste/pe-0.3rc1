7df8e2ae2d8064e36d87162a52426c3f1186729c
superman <raphael.muenster@math.tu-dortmund.de>
2023-06-13 14:39:00 +0200

    Added new distribution functions to bench setup

--- pe/interface/setup_bench.h
+++ pe/interface/setup_bench.h
@@ -1,10 +1,66 @@
 #include <pe/interface/decompose.h>
 #include <random>
+#include <algorithm>
+
+
+//*************************************************************************************************
+/*!\brief Generates a mesh
+ *
+ * \param minR Radius of inner circle
+ * \param maxR Radius of outer circle 
+ * \param dR Radius increment
+ * \param dP Distance between particles
+ * \param dZ Distance between two layers
+ *
+ */
+std::vector<Vec3> generateMeshPoints(real minR, real maxR, real dR, real dP, real dZ) {
+  std::vector<Vec3> meshPoints;
+
+  real mz = 0.0;
+  for (int i = 0; i < 5; i++) {
+    for (real r = minR; r <= maxR; r += dR) {
+      real circumference = 2. * M_PI * r;
+
+      int numPoints = std::round(circumference / dP);
+
+      // for each points on the circle
+      for (int j = 0; j < numPoints; j++) {
+        real angle = (real)j / numPoints * 2 * M_PI;
+        real x = r * std::cos(angle);
+        real y = r * std::sin(angle);
+
+        meshPoints.push_back(Vec3(x, y, mz));
+      }
+    }
+    mz += dZ;
+  }
+  return meshPoints;
+}
+
+std::vector<int> generateUniqueRandomIntegers(int vecSize, int numValues) {
+
+  std::vector<int> indices(vecSize);
+  for(int i = 0; i < vecSize; ++i) {
+    indices[i] = i;
+  }
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+
+  // Shuffle the indices vector
+  std::shuffle(indices.begin(), indices.end(), gen);
+
+  // Get the unique random values
+  int numRandomValues = std::min(numValues, vecSize);
+  std::vector<int> randomValues(indices.begin(), indices.begin() + numRandomValues);
+
+  return randomValues;
+}
 
 real generateRandomNumber() {
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_real_distribution<double> dis(0.0, 0.1);
+  std::uniform_real_distribution<double> dis(-0.01, 0.1);
   return dis(gen);
 }
 
@@ -148,20 +204,43 @@ void setupBench(MPI_Comm ex0) {
   int idx = 0;
 
   // Create a custom material for the benchmark
-  MaterialID myMaterial = createMaterial("Bench", 1.1, 0.0, 0.1, 0.05, 0.2, 80, 100, 10, 11);
+  MaterialID myMaterial = createMaterial("Bench", 1.0, 0.0, 0.1, 0.05, 0.2, 80, 100, 10, 11);
 
   real h = 0.0075;
   //0.015
-  real radius2 = 2. * h;
+  //real radius2 = 2. * h;
+  real radius2 = 0.02;
+
   Vec3 position( 0.25,-0.25, 0.2);
+  //
+//*************************************************************************************************
+/*!\brief Generates a mesh
+ *
+ * \param minR Radius of inner circle
+ * \param maxR Radius of outer circle 
+ * \param dR Radius increment
+ * \param dP Distance between particles
+ * \param dZ Distance between two layers
+ *
+ */
+//std::vector<Vec3> generateMeshPoints(real minR, real maxR, real dR, real dP, real dZ) {
+
+  std::vector<int> sampleLocation;
+
+  //cartcomm
+
+  pe_EXCLUSIVE_SECTION( 0 ) {
+//    sampleLocation = generateUniqueRandomIntegers(int vecSize, int numValues);
+  }
 
   //=========================================================================================
-
-    const double initialRadius = 0.25;
-    const double distance = 0.03;
-    const int numIterations = 1;
-    const double radiusIncrement = 0.015 + distance;
+    const double initialRadius = 0.262;
+    const double distance = 2.5 * radius2;
+    const int numIterations = 3;
+    const double radiusIncrement = radius2 + 0.5 * distance;
+    int hZ[] = {5, 9, 9};
     
+    int count(0);
     bool resume = true;
     if(!resume) {
       for (int iteration = 0; iteration < numIterations; ++iteration) {
@@ -170,24 +249,38 @@ void setupBench(MPI_Comm ex0) {
         int numPoints = static_cast<int>(2 * M_PI * radius / distance);
         double angleIncrement = 2 * M_PI / numPoints;        
         real pos_z = radius2 + h;
-        real zinc = 2. * radius2 + 0.0015;
-
+        real zinc = 2. * radius2 + 0.002;
+  pe_EXCLUSIVE_SECTION( 0 ) {
+    std::cout << "\n--" << "numPoints" << numPoints << std::endl;
+  }
+        int height = hZ[iteration];
         int j = 0;
-        for (; j < 11; ++j) {
+        for (; j < height; ++j) {
           for (int i = 0; i < numPoints; ++i) {
             double angle = i * angleIncrement;
             position[0] = radius * std::cos(angle);
             position[1] = radius * std::sin(angle);
             position[2] = pos_z + j * zinc;
-            real r = generateRandomNumber();
-            Vec3 add = r * position;
-            add[2] = 0.0;
-            position += add;
+////            if (i != numPoints -1) {
+//            real r = generateRandomNumber();
+//            Vec3 add = r * position;
+//            add[2] = 0.0;
+//            position += add;
+////            position[0] += r;
+////            r = generateRandomNumber();
+////            position[1] += r;
+////            r = generateRandomNumber();
+////            position[2] += r;
+////            }
 
-            if (world->ownsPoint( position )) {
-              SphereID sphere = createSphere(idx, position, radius2, myMaterial, true);
-              ++idx;      
-            }
+
+//            if(count < 112) {
+              if (world->ownsPoint( position )) {
+                SphereID sphere = createSphere(idx, position, radius2, myMaterial, true);
+                ++idx;      
+              }
+//            }
+            count++;
           }
         }        
       }
@@ -244,7 +337,7 @@ void setupBench(MPI_Comm ex0) {
   CylinderID cyl(0);
   pe_GLOBAL_SECTION
   {
-    cyl = createCylinder( 10011, 0.0, 0.0, 0.2, 0.2, 0.4, iron );
+    cyl = createCylinder( 10011, 0.0, 0.0, 0.2, 0.22, 0.4, iron );
     cyl->setFixed(true);
     cyl->rotate(0.0, M_PI/2.0 , 0.0);
 
@@ -291,6 +384,7 @@ void setupBench(MPI_Comm ex0) {
       << " particles y              = " << ny << "\n" 
       << " particles z              = " << nz << "\n" 
       << " Total particles          = " << particlesTotal << "\n"
+      << " particle volume          = " << partVol << "\n"
       << " Domain volume            = " << domainVol << "\n"
       << " Volume fraction[%]       = " << (particlesTotal * partVol)/domainVol * 100.0 << "\n"
       << " Total objects            = " << primitivesTotal << "\n" << std::endl;
