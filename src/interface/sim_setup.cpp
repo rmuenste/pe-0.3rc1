85981c9eed8b93fb7a80a72a4d42784dedae5bba
superman <raphael.muenster@math.tu-dortmund.de>
2024-02-02 22:39:57 +0100

    changes for span III

--- src/interface/sim_setup.cpp
+++ src/interface/sim_setup.cpp
@@ -31,7 +31,7 @@ const real   velocity( 0.0025 );  // Initial maximum velocity of the spheres
 const size_t initsteps     (  20000 );  // Initialization steps with closed outlet door
 const size_t focussteps    (    100 );  // Number of initial close-up time steps
 const size_t animationsteps(    200 );  // Number of time steps for the camera animation
-const size_t timesteps     ( 3500 );  // Number of time steps for the flowing granular media
+const size_t timesteps     ( 10000 );  // Number of time steps for the flowing granular media
 const real   stepsize      (  0.00001 );  // Size of a single time step
 
 // Process parameters
