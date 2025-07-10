1dd12db4f1902eb8eb68cbd580f3c66714e055d5
Raphael Münster <raphael.muenster@tu-dortmund.de>
2025-07-10 15:50:33 +0200

    Revitalized the dkt example

--- pe/interface/setup_dkt.h
+++ pe/interface/setup_dkt.h
@@ -1,12 +1,17 @@
 
+#include <pe/interface/decompose.h>
+
 void setupDraftKissTumbBench(MPI_Comm ex0) {
 
   auto& config = SimulationConfig::getInstance();
   world = theWorld();
-  world->setGravity( 0.0, 0.0, -98.1 );
+  
+  loadSimulationConfig("example.json");
+  
+  world->setGravity( config.getGravity() );
   world->setLiquidSolid(true);
-  world->setLiquidDensity(1.0);
-
+  world->setLiquidDensity( config.getFluidDensity() );
+  world->setViscosity( config.getFluidViscosity() );
   world->setDamping( 1.0 );
 
   // Configuration of the MPI system
@@ -15,9 +20,7 @@ void setupDraftKissTumbBench(MPI_Comm ex0) {
 
   const real dx( 2.00 );
   const real dy( 2.00 );
-  //const real dz( 0.08 ) 2 subs;
-  //const real dz( 0.08 ) 2 subs;
-  const real dz( 8.0 / 16.0 );
+  const real dz( 8.0 / config.getProcessesZ() );
 
   int my_rank;
   MPI_Comm_rank(ex0, &my_rank);
@@ -88,297 +91,8 @@ void setupDraftKissTumbBench(MPI_Comm ex0) {
   real by = 0.0;
   real bz = 0.0;
 
-  int west     [] = { center[0]-1, center[1]  , center[2] };
-  int east     [] = { center[0]+1, center[1]  , center[2] };
-  int south    [] = { center[0]  , center[1]-1, center[2] };
-  int north    [] = { center[0]  , center[1]+1, center[2] };
-  int southwest[] = { center[0]-1, center[1]-1, center[2] };
-  int southeast[] = { center[0]+1, center[1]-1, center[2] };
-  int northwest[] = { center[0]-1, center[1]+1, center[2] };
-  int northeast[] = { center[0]+1, center[1]+1, center[2] };
-
-  // bottom
-  int bottom         [] = { center[0]  , center[1]  , center[2]-1 };
-  int bottomwest     [] = { center[0]-1, center[1]  , center[2]-1 };
-  int bottomeast     [] = { center[0]+1, center[1]  , center[2]-1 };
-  int bottomsouth    [] = { center[0]  , center[1]-1, center[2]-1 };
-  int bottomnorth    [] = { center[0]  , center[1]+1, center[2]-1 };
-  int bottomsouthwest[] = { center[0]-1, center[1]-1, center[2]-1 };
-  int bottomsoutheast[] = { center[0]+1, center[1]-1, center[2]-1 };
-  int bottomnorthwest[] = { center[0]-1, center[1]+1, center[2]-1 };
-  int bottomnortheast[] = { center[0]+1, center[1]+1, center[2]-1 };
-
-  // top
-  int top         [] = { center[0]  , center[1]  , center[2]+1 };
-  int topwest     [] = { center[0]-1, center[1]  , center[2]+1 };
-  int topeast     [] = { center[0]+1, center[1]  , center[2]+1 };
-  int topsouth    [] = { center[0]  , center[1]-1, center[2]+1 };
-  int topnorth    [] = { center[0]  , center[1]+1, center[2]+1 };
-  int topsouthwest[] = { center[0]-1, center[1]-1, center[2]+1 };
-  int topsoutheast[] = { center[0]+1, center[1]-1, center[2]+1 };
-  int topnorthwest[] = { center[0]-1, center[1]+1, center[2]+1 };
-  int topnortheast[] = { center[0]+1, center[1]+1, center[2]+1 };
-
-   // Specify local domain
-   defineLocalDomain( intersect(
-      intersect(
-      HalfSpace( Vec3(+1,0,0), +(bx+center[0]*dx) ),
-      HalfSpace( Vec3(-1,0,0), -(bx+east[0]*dx  ) ) ),
-      HalfSpace( Vec3(0,+1,0), +(by+center[1]*dy) ),
-      HalfSpace( Vec3(0,-1,0), -(by+north[1]*dy ) ),
-      HalfSpace( Vec3(0,0,+1), +(bz+center[2]*dz) ),
-      HalfSpace( Vec3(0,0,-1), -(bz+top[2]*dz ) ) ) );
-
-   // Connecting the west neighbor
-   if( west[0] >= 0 ) {
-      MPI_Cart_rank( cartcomm, west, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(-1,0,0), -(bx+center[0]*dx) ),
-         HalfSpace( Vec3(0,+1,0), +(by+center[1]*dy) ),
-         HalfSpace( Vec3(0,-1,0), -(by+north[1]*dy ) ),
-         HalfSpace( Vec3(0,0,+1), +(bz+center[2]*dz) ),
-         HalfSpace( Vec3(0,0,-1), -(bz+top[2]*dz   ) ) ) );
-   }
-
-   // Connecting the east neighbor
-   if( east[0] < px ) {
-      MPI_Cart_rank( cartcomm, east, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(+1,0,0), +(bx+east[0]*dx  ) ),
-         HalfSpace( Vec3(0,+1,0), +(by+center[1]*dy) ),
-         HalfSpace( Vec3(0,-1,0), -(by+north[1]*dy ) ),
-         HalfSpace( Vec3(0,0,+1), +(bz+center[2]*dz) ),
-         HalfSpace( Vec3(0,0,-1), -(bz+top[2]*dz   ) ) ) );
-   }
-
-   // Connecting the south neighbor
-   if( south[1] >= 0 ) {
-      MPI_Cart_rank( cartcomm, south, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(0,-1,0), -(by+center[1]*dy) ),
-         HalfSpace( Vec3(+1,0,0), +(bx+center[0]*dx) ),
-         HalfSpace( Vec3(-1,0,0), -(bx+east[0]*dx  ) ),
-         HalfSpace( Vec3(0,0,+1), +(bz+center[2]*dz) ),
-         HalfSpace( Vec3(0,0,-1), -(bz+top[2]*dz   ) ) ) );
-   }
-
-   // Connecting the north neighbor
-   if( north[1] < py ) {
-      MPI_Cart_rank( cartcomm, north, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(0,+1,0), +(by+north[1]*dy ) ),
-         HalfSpace( Vec3(+1,0,0), +(bx+center[0]*dx) ),
-         HalfSpace( Vec3(-1,0,0), -(bx+east[0]*dx  ) ),
-         HalfSpace( Vec3(0,0,+1), +(bz+center[2]*dz) ),
-         HalfSpace( Vec3(0,0,-1), -(bz+top[2]*dz   ) ) ) );
-   }
-
-   // Connecting the bottom neighbor
-   if( bottom[2] >= 0 ) {
-      MPI_Cart_rank( cartcomm, bottom, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(0,0,-1), -(bz+center[2]*dz) ),
-         HalfSpace( Vec3(+1,0,0), +(bx+center[0]*dx) ),
-         HalfSpace( Vec3(-1,0,0), -(bx+east[0]*dx  ) ),
-         HalfSpace( Vec3(0,+1,0), +(by+center[1]*dy) ),
-         HalfSpace( Vec3(0,-1,0), -(by+north[1]*dy ) ) ) );
-   }
-
-   // Connecting the top neighbor
-   if( top[2] < pz ) {
-      MPI_Cart_rank( cartcomm, top, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(0,0,+1), +(bz+top[2]*dz   ) ),
-         HalfSpace( Vec3(+1,0,0), +(bx+center[0]*dx) ),
-         HalfSpace( Vec3(-1,0,0), -(bx+east[0]*dx  ) ),
-         HalfSpace( Vec3(0,+1,0), +(by+center[1]*dy) ),
-         HalfSpace( Vec3(0,-1,0), -(by+north[1]*dy ) ) ) );
-   }
-
-   // Connecting the south-west neighbor
-   if( southwest[0] >= 0 && southwest[1] >= 0 ) {
-      MPI_Cart_rank( cartcomm, southwest, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(-1,0,0), -(bz+center[0]*dx) ),
-         HalfSpace( Vec3(0,-1,0), -(by+center[1]*dy) ),
-         HalfSpace( Vec3(0,0,+1), +(bz+center[2]*dz) ),
-         HalfSpace( Vec3(0,0,-1), -(bz+top[2]*dz   ) ) ) );
-   }
-
-
-   // Connecting the south-east neighbor
-   if( southeast[0] < px && southeast[1] >= 0 ) {
-      MPI_Cart_rank( cartcomm, southeast, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(+1,0,0), +(bx+east[0]*dx  ) ),
-         HalfSpace( Vec3(0,-1,0), -(by+center[1]*dy) ),
-         HalfSpace( Vec3(0,0,+1), +(bz+center[2]*dz) ),
-         HalfSpace( Vec3(0,0,-1), -(bz+top[2]*dz   ) ) ) );
-   }
-
-   // Connecting the north-west neighbor
-   if( northwest[0] >= 0 && northwest[1] < py ) {
-      MPI_Cart_rank( cartcomm, northwest, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(-1,0,0), -(bx+center[0]*dx) ),
-         HalfSpace( Vec3(0,+1,0), +(by+north[1]*dy ) ),
-         HalfSpace( Vec3(0,0,+1), +(bz+center[2]*dz) ),
-         HalfSpace( Vec3(0,0,-1), -(bz+top[2]*dz   ) ) ) );
-   }
-
-   // Connecting the north-east neighbor
-   if( northeast[0] < px && northeast[1] < py ) {
-      MPI_Cart_rank( cartcomm, northeast, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(+1,0,0), +(bx+east[0]*dx  ) ),
-         HalfSpace( Vec3(0,+1,0), +(by+north[1]*dy ) ),
-         HalfSpace( Vec3(0,0,+1), +(bz+center[2]*dz) ),
-         HalfSpace( Vec3(0,0,-1), -(bz+top[2]*dz   ) ) ) );
-   }
-
-   // Connecting the bottom-west neighbor
-   if( bottomwest[0] >= 0 && bottomwest[2] >= 0 ) {
-      MPI_Cart_rank( cartcomm, bottomwest, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(-1,0,0), -(bx+center[0]*dx) ),
-         HalfSpace( Vec3(0,0,-1), -(bz+center[2]*dz) ),
-         HalfSpace( Vec3(0,+1,0), +(by+center[1]*dy) ),
-         HalfSpace( Vec3(0,-1,0), -(by+north[1]*dy ) ) ) );
-   }
-
-   // Connecting the bottom-east neighbor
-   if( bottomeast[0] < px && bottomeast[2] >= 0 ) {
-      MPI_Cart_rank( cartcomm, bottomeast, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(+1,0,0), +(bx+east[0]*dx  ) ),
-         HalfSpace( Vec3(0,0,-1), -(bz+center[2]*dz) ),
-         HalfSpace( Vec3(0,+1,0), +(by+center[1]*dy) ),
-         HalfSpace( Vec3(0,-1,0), -(by+north[1]*dy ) ) ) );
-   }
-
-   // Connecting the bottom-south neighbor
-   if( bottomsouth[1] >= 0 && bottomsouth[2] >= 0 ) {
-      MPI_Cart_rank( cartcomm, bottomsouth, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(0,-1,0), -(by+center[1]*dy) ),
-         HalfSpace( Vec3(0,0,-1), -(bz+center[2]*dz) ),
-         HalfSpace( Vec3(+1,0,0), +(bx+center[0]*dx) ),
-         HalfSpace( Vec3(-1,0,0), -(bx+east[0]*dx  ) ) ) );
-   }
-
-   // Connecting the bottom-north neighbor
-   if( bottomnorth[1] < py && bottomnorth[2] >= 0 ) {
-      MPI_Cart_rank( cartcomm, bottomnorth, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(0,+1,0), +(by+north[1]*dy ) ),
-         HalfSpace( Vec3(0,0,-1), -(bz+center[2]*dz) ),
-         HalfSpace( Vec3(+1,0,0), +(bx+center[0]*dx) ),
-         HalfSpace( Vec3(-1,0,0), -(bx+east[0]*dx  ) ) ) );
-   }
-
-   // Connecting the bottom-south-west neighbor
-   if( bottomsouthwest[0] >= 0 && bottomsouthwest[1] >= 0 && bottomsouthwest[2] >= 0 ) {
-      MPI_Cart_rank( cartcomm, bottomsouthwest, &rank );
-      connect( rank, intersect( HalfSpace( Vec3(-1,0,0), -(bx+center[0]*dx) ),
-                                HalfSpace( Vec3(0,-1,0), -(by+center[1]*dy) ),
-                                HalfSpace( Vec3(0,0,-1), -(bz+center[2]*dz) ) ) );
-   }
-
-   // Connecting the bottom-south-east neighbor
-   if( bottomsoutheast[0] < px && bottomsoutheast[1] >= 0 && bottomsoutheast[2] >= 0 ) {
-      MPI_Cart_rank( cartcomm, bottomsoutheast, &rank );
-      connect( rank, intersect( HalfSpace( Vec3(1,0,0),  +(bx+east[0]*dx  ) ),
-                                HalfSpace( Vec3(0,-1,0), -(by+center[1]*dy) ),
-                                HalfSpace( Vec3(0,0,-1), -(bz+center[2]*dz) ) ) );
-   }
-
-   // Connecting the bottom-north-west neighbor
-   if( bottomnorthwest[0] >= 0 && bottomnorthwest[1] < py && bottomnorthwest[2] >= 0 ) {
-      MPI_Cart_rank( cartcomm, bottomnorthwest, &rank );
-      connect( rank, intersect( HalfSpace( Vec3(-1,0,0), -(bx+center[0]*dx) ),
-                                HalfSpace( Vec3(0,1,0),  +(by+north[1]*dy ) ),
-                                HalfSpace( Vec3(0,0,-1), -(bz+center[2]*dz) ) ) );
-   }
-
-   // Connecting the bottom-north-east neighbor
-   if( bottomnortheast[0] < px && bottomnortheast[1] < py && bottomnortheast[2] >= 0 ) {
-      MPI_Cart_rank( cartcomm, bottomnortheast, &rank );
-      connect( rank, intersect( HalfSpace( Vec3(1,0,0),  +(bx+east[0]*dx  ) ),
-                                HalfSpace( Vec3(0,1,0),  +(by+north[1]*dy ) ),
-                                HalfSpace( Vec3(0,0,-1), -(bz+center[2]*dz) ) ) );
-   }
-
-   // Connecting the top-west neighbor
-   if( topwest[0] >= 0 && topwest[2] < pz ) {
-      MPI_Cart_rank( cartcomm, topwest, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(-1,0,0), -(bx+center[0]*dx) ),
-         HalfSpace( Vec3(0,0,1),  +(bz+top[2]*dz   ) ),
-         HalfSpace( Vec3(0,+1,0), +(by+center[1]*dy) ),
-         HalfSpace( Vec3(0,-1,0), -(by+north[1]*dy ) ) ) );
-   }
-
-   // Connecting the top-east neighbor
-   if( topeast[0] < px && topeast[2] < pz ) {
-      MPI_Cart_rank( cartcomm, topeast, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(1,0,0),  +(bx+east[0]*dx  ) ),
-         HalfSpace( Vec3(0,0,1),  +(bz+top[2]*dz   ) ),
-         HalfSpace( Vec3(0,+1,0), +(by+center[1]*dy) ),
-         HalfSpace( Vec3(0,-1,0), -(by+north[1]*dy ) ) ) );
-   }
-
-   // Connecting the top-south neighbor
-   if( topsouth[1] >= 0 && topsouth[2] < pz ) {
-      MPI_Cart_rank( cartcomm, topsouth, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(0,-1,0), -(by+center[1]*dy) ),
-         HalfSpace( Vec3(0,0,1),  +(bz+top[2]*dz   ) ),
-         HalfSpace( Vec3(+1,0,0), +(bx+center[0]*dx) ),
-         HalfSpace( Vec3(-1,0,0), -(bx+east[0]*dx  ) ) ) );
-   }
-
-   // Connecting the top-north neighbor
-   if( topnorth[1] < py && topnorth[2] < pz ) {
-      MPI_Cart_rank( cartcomm, topnorth, &rank );
-      connect( rank, intersect(
-         HalfSpace( Vec3(0,1,0),  +(by+north[1]*dy ) ),
-         HalfSpace( Vec3(0,0,1),  +(bz+top[2]*dz   ) ),
-         HalfSpace( Vec3(+1,0,0), +(bx+center[0]*dx) ),
-         HalfSpace( Vec3(-1,0,0), -(bx+east[0]*dx  ) ) ) );
-   }
-
-   // Connecting the top-south-west neighbor
-   if( topsouthwest[0] >= 0 && topsouthwest[1] >= 0 && topsouthwest[2] < pz ) {
-      MPI_Cart_rank( cartcomm, topsouthwest, &rank );
-      connect( rank, intersect( HalfSpace( Vec3(-1,0,0), -(bx+center[0]*dx) ),
-                                HalfSpace( Vec3(0,-1,0), -(by+center[1]*dy) ),
-                                HalfSpace( Vec3(0,0,1),  +(bz+top[2]*dz   ) ) ) );
-   }
-
-   // Connecting the top-south-east neighbor
-   if( topsoutheast[0] < px && topsoutheast[1] >= 0 && topsoutheast[2] < pz ) {
-      MPI_Cart_rank( cartcomm, topsoutheast, &rank );
-      connect( rank, intersect( HalfSpace( Vec3(1,0,0),  +(bx+east[0]*dx  ) ),
-                                HalfSpace( Vec3(0,-1,0), -(by+center[1]*dy) ),
-                                HalfSpace( Vec3(0,0,1),  +(bz+top[2]*dz   ) ) ) );
-   }
-
-   // Connecting the top-north-west neighbor
-   if( topnorthwest[0] >= 0 && topnorthwest[1] < py && topnorthwest[2] < pz ) {
-      MPI_Cart_rank( cartcomm, topnorthwest, &rank );
-      connect( rank, intersect( HalfSpace( Vec3(-1,0,0), -(bx+center[0]*dx) ),
-                                HalfSpace( Vec3(0,1,0),  +(by+north[1]*dy ) ),
-                                HalfSpace( Vec3(0,0,1),  +(bz+top[2]*dz   ) ) ) );
-   }
-
-   // Connecting the top-north-east neighbor
-   if( topnortheast[0] < px && topnortheast[1] < py && topnortheast[2] < pz ) {
-      MPI_Cart_rank( cartcomm, topnortheast, &rank );
-      connect( rank, intersect( HalfSpace( Vec3(1,0,0), +(bx+east[0]*dx ) ),
-                                HalfSpace( Vec3(0,1,0), +(by+north[1]*dy) ),
-                                HalfSpace( Vec3(0,0,1), +(bz+top[2]*dz  ) ) ) );
-   }
+  // Setup domain decomposition using decomposeDomain function for non-periodic 3D
+  decomposeDomain(center, bx, by, bz, dx, dy, dz, px, py, pz);
 
 //===========================================================================================================
 
@@ -388,7 +102,7 @@ void setupDraftKissTumbBench(MPI_Comm ex0) {
    theMPISystem()->checkProcesses();
 //#endif
 
-  MaterialID gr = createMaterial("ground", 1120.0, 0.0, 0.1, 0.05, 0.2, 80, 100, 10, 11);
+  MaterialID gr = createMaterial("ground", config.getParticleDensity(), 0.0, 0.1, 0.05, 0.2, 80, 100, 10, 11);
   std::cout << "[Creating a plane] " << std::endl;
   pe_GLOBAL_SECTION
   {
@@ -433,12 +147,10 @@ void setupDraftKissTumbBench(MPI_Comm ex0) {
   int idx = 0;
 
   // Create a custom material for the benchmark
-  MaterialID myMaterial = createMaterial("Bench", 1.1, 0.0, 0.1, 0.05, 0.2, 80, 100, 10, 11);
+  MaterialID myMaterial = createMaterial("Bench", config.getParticleDensity(), 0.0, 0.1, 0.05, 0.2, 80, 100, 10, 11);
   Vec3 position( 0.99,  1.0, 6.9);
-//  0.123597
-
-//  real radBench = 0.005;
-  real radBench = 0.1;
+  
+  real radBench = config.getBenchRadius();
   if (world->ownsPoint( position )) {
     SphereID spear = createSphere(idx, position, radBench, myMaterial, true);
     std::cout << "[Creating particle] at: " << (position) << " in domain: " << my_rank << std::endl;
@@ -467,6 +179,7 @@ void setupDraftKissTumbBench(MPI_Comm ex0) {
   unsigned long bodiesUpdate = static_cast<unsigned long>(numBodies);
   MPI_Reduce( &bla, &particlesTotal, 1, MPI_UNSIGNED_LONG, MPI_SUM, 0, cartcomm );
   MPI_Reduce( &bodiesUpdate, &primitivesTotal, 1, MPI_UNSIGNED_LONG, MPI_SUM, 0, cartcomm );
+  TimeStep::stepsize( config.getStepsize() );
 
   pe_EXCLUSIVE_SECTION( 0 ) {
     std::cout << "\n--" << "SIMULATION SETUP"
